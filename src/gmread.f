c2534150fb924439667e46f446c3e343578f3eda
Yongcheng Wu <yc_wu1030@126.com>
2019-05-15 11:34:21 -0400

    BUG FIX
    Devided-by-Zero problem in INPUTSET=2 when calculating the mass matrix
    for CP-Even singlets

--- src/gmread.f
+++ src/gmread.f
@@ -185,7 +185,7 @@ C Solve the cubic equation for v_chi.
       U1 = (1.D0,0.D0)
       U2 = -0.5D0 + II*DSQRT(3.D0)/2.D0
       U3 = -0.5D0 - II*DSQRT(3.D0)/2.D0
-      ALPHA = -48.D0*LAMBDA2 + 24.D0*LAMBDA5 + 12.D0*LAMBDA3 
+      ALPHA = -48.D0*LAMBDA2 + 24.D0*LAMBDA5 + 12.D0*LAMBDA3
      .     + 36.D0*LAMBDA4
       BETA = 6.D0*M1 - 18.D0*M2
       GAMMA = 3.D0*MU3SQ + (6.D0*LAMBDA2 - 3.D0*LAMBDA5)*V**2
@@ -194,7 +194,7 @@ C Solve the cubic equation for v_chi.
      .     + BETA**2 * GAMMA**2 - 4.D0 * ALPHA * GAMMA**3
      .     - 27.D0 * ALPHA**2 * DELTA**2
       DISC0 = BETA**2 - 3.D0 * ALPHA * GAMMA
-      DISC1 = 2.D0 * BETA**3 - 9.D0 * ALPHA*BETA*GAMMA 
+      DISC1 = 2.D0 * BETA**3 - 9.D0 * ALPHA*BETA*GAMMA
      .     + 27.D0 * ALPHA**2 * DELTA
       CROOT = (0.5D0 * DISC1 + 0.5D0*CDSQRT(-27.D0*ALPHA**2*DISC)
      .     )**(1.D0/3.D0)
@@ -212,7 +212,7 @@ C Check that v_chi is not too big for each solution.
             IF (8.D0*VCHI(J)**2.LT.V**2) THEN
                SOLOK(J) = 1
                VPHI(J) = DSQRT(V**2 - 8.D0*VCHI(J)**2)
-               MU2SQTEST(J) = 2.D0*(-2.D0*LAMBDA1*VPHI(J)**2 
+               MU2SQTEST(J) = 2.D0*(-2.D0*LAMBDA1*VPHI(J)**2
      .              - (3.D0*LAMBDA2 - 3.D0/2.D0*LAMBDA5)*VCHI(J)**2
      .              + 3.D0/4.D0*M1*VCHI(J))
             ELSE
@@ -220,7 +220,7 @@ C Check that v_chi is not too big for each solution.
                MU2SQTEST(J) = 0.D0
             ENDIF
  10      CONTINUE
-C Choose among the 3 solutions: check whether each v_chi is actually the 
+C Choose among the 3 solutions: check whether each v_chi is actually the
 C minimum for this set of Lagrangian parameters, using GETMINIMA.
          MU2SQ = 0.D0
          INPUTOK = 0
@@ -273,10 +273,10 @@ C Check that the one solution v_chi is not too big.
          IF (8.D0*VCHI(1)**2.LT.V**2) THEN
 C Now compute mu2^2.
             VPHI(1) = DSQRT(V**2 - 8.D0*VCHI(1)**2)
-            MU2SQ = 2.D0*(-2.D0*LAMBDA1*VPHI(1)**2 
+            MU2SQ = 2.D0*(-2.D0*LAMBDA1*VPHI(1)**2
      .           - (3.D0*LAMBDA2 - 3.D0/2.D0*LAMBDA5)*VCHI(1)**2
      .           + 3.D0/4.D0*M1*VCHI(1))
-C Check whether v_chi is actually the minimum for this set of Lagrangian 
+C Check whether v_chi is actually the minimum for this set of Lagrangian
 C parameters, using GETMINIMA.
             CALL GETMINIMA
 C Identify the deepest of the good extrema:
@@ -347,7 +347,7 @@ C Local variables:
       DOUBLE COMPLEX DISC, DISC0, DISC1
       DOUBLE COMPLEX II, U1, U2, U3, CROOT, CVCHI1, CVCHI2, CVCHI3
       INTEGER J
-      
+
       IF (INPUTMODE.EQ.1) THEN
 C Read inputs from the user (interactive mode)
       PRINT *
@@ -364,7 +364,7 @@ C Solve the cubic equation for v_chi.
       U1 = (1.D0,0.D0)
       U2 = -0.5D0 + II*DSQRT(3.D0)/2.D0
       U3 = -0.5D0 - II*DSQRT(3.D0)/2.D0
-      ALPHA = -48.D0*LAMBDA2 + 24.D0*LAMBDA5 + 12.D0*LAMBDA3 
+      ALPHA = -48.D0*LAMBDA2 + 24.D0*LAMBDA5 + 12.D0*LAMBDA3
      .     + 36.D0*LAMBDA4
       BETA = 6.D0*M1 - 18.D0*M2
       GAMMA = 3.D0*MU3SQ + (6.D0*LAMBDA2 - 3.D0*LAMBDA5)*V**2
@@ -373,7 +373,7 @@ C Solve the cubic equation for v_chi.
      .     + BETA**2 * GAMMA**2 - 4.D0 * ALPHA * GAMMA**3
      .     - 27.D0 * ALPHA**2 * DELTA**2
       DISC0 = BETA**2 - 3.D0 * ALPHA * GAMMA
-      DISC1 = 2.D0 * BETA**3 - 9.D0 * ALPHA*BETA*GAMMA 
+      DISC1 = 2.D0 * BETA**3 - 9.D0 * ALPHA*BETA*GAMMA
      .     + 27.D0 * ALPHA**2 * DELTA
       CROOT = (0.5D0 * DISC1 + 0.5D0*CDSQRT(-27.D0*ALPHA**2*DISC)
      .     )**(1.D0/3.D0)
@@ -392,16 +392,16 @@ C Check that v_chi is not too big for each solution.
                SOLOK(J) = 1
                VPHI(J) = DSQRT(V**2 - 8.D0*VCHI(J)**2)
 C Compute lambda1 and mu2sq for each solution:
-               MSQ12 = DSQRT(3.D0)*VPHI(J) * (-M1/2.D0 
+               MSQ12 = DSQRT(3.D0)*VPHI(J) * (-M1/2.D0
      .              + (4.D0*LAMBDA2 - 2.D0*LAMBDA5)*VCHI(J))
 C               MSQ22 = M1*VPHI(J)**2/4.D0/VCHI(J) - 6.D0*M2*VCHI(J)
 C     .              + (8.D0*LAMBDA3 + 24.D0*LAMBDA4)*VCHI(J)**2
-               MSQ22 = MU3SQ - 12.D0*M2*VCHI(J) 
+               MSQ22 = MU3SQ - 12.D0*M2*VCHI(J)
      .          + (2.D0*LAMBDA2-LAMBDA5)*VPHI(J)**2
      .          + 12.D0*(LAMBDA3+3.D0*LAMBDA4)*VCHI(J)**2
-               L1TEST(J) = 1.D0/8.D0/VPHI(J)**2 
+               L1TEST(J) = 1.D0/8.D0/VPHI(J)**2
      .              * (MH**2 + MSQ12**2/(MSQ22 - MH**2))
-               MU2SQTEST(J) = 2.D0*(-2.D0*L1TEST(J)*VPHI(J)**2 
+               MU2SQTEST(J) = 2.D0*(-2.D0*L1TEST(J)*VPHI(J)**2
      .              - (3.D0*LAMBDA2 - 3.D0/2.D0*LAMBDA5)*VCHI(J)**2
      .              + 3.D0/4.D0*M1*VCHI(J))
             ELSE
@@ -410,7 +410,7 @@ C     .              + (8.D0*LAMBDA3 + 24.D0*LAMBDA4)*VCHI(J)**2
                MU2SQTEST(J) = 0.D0
             ENDIF
  10      CONTINUE
-C Choose among the 3 solutions: check whether each v_chi is actually the 
+C Choose among the 3 solutions: check whether each v_chi is actually the
 C minimum for this set of Lagrangian parameters, using GETMINIMA.
          LAMBDA1 = 0.D0
          MU2SQ = 0.D0
@@ -466,19 +466,19 @@ C Check that the one solution v_chi is not too big.
          IF (8.D0*VCHI(1)**2.LT.V**2) THEN
 C Now compute lambda1 and mu2^2.
             VPHI(1) = DSQRT(V**2 - 8.D0*VCHI(1)**2)
-            MSQ12 = DSQRT(3.D0)*VPHI(1) * (-M1/2.D0 
+            MSQ12 = DSQRT(3.D0)*VPHI(1) * (-M1/2.D0
      .           + (4.D0*LAMBDA2 - 2.D0*LAMBDA5)*VCHI(1))
 C             MSQ22 = M1*VPHI(1)**2/4.D0/VCHI(1) - 6.D0*M2*VCHI(1)
 C      .           + (8.D0*LAMBDA3 + 24.D0*LAMBDA4)*VCHI(1)**2
-            MSQ22 = MU3SQ - 12.D0*M2*VCHI(1) 
+            MSQ22 = MU3SQ - 12.D0*M2*VCHI(1)
      .          + (2.D0*LAMBDA2-LAMBDA5)*VPHI(1)**2
      .          + 12.D0*(LAMBDA3+3.D0*LAMBDA4)*VCHI(1)**2
-            LAMBDA1 = 1.D0/8.D0/VPHI(1)**2 
+            LAMBDA1 = 1.D0/8.D0/VPHI(1)**2
      .              * (MH**2 + MSQ12**2/(MSQ22 - MH**2))
-            MU2SQ = 2.D0*(-2.D0*LAMBDA1*VPHI(1)**2 
+            MU2SQ = 2.D0*(-2.D0*LAMBDA1*VPHI(1)**2
      .           - (3.D0*LAMBDA2 - 3.D0/2.D0*LAMBDA5)*VCHI(1)**2
      .           + 3.D0/4.D0*M1*VCHI(1))
-C Check whether v_chi is actually the minimum for this set of Lagrangian 
+C Check whether v_chi is actually the minimum for this set of Lagrangian
 C parameters, using GETMINIMA.
             CALL GETMINIMA
 C Identify the deepest of the good extrema:
@@ -512,7 +512,7 @@ C Identify the deepest of the good extrema:
 
 C==================================================================
       SUBROUTINE READIN3
-C Reads in the inputs for Input Set 3 and computes the rest of the 
+C Reads in the inputs for Input Set 3 and computes the rest of the
 C Lagrangian parameters in LPARAMS.
 C Input Set 3 is:
 C     mh, mH, m3, m5, sin(thetaH), sin(alpha), M1, M2
@@ -561,7 +561,7 @@ C Read inputs from the user (interactive mode)
       MH5 = IMH5
       SH = ISH
       SA = ISA
-      
+
       INPUTOK = 1
 
       IF (SH.GE.1.D0.OR.SH.LE.0.D0) THEN
@@ -581,14 +581,14 @@ C Read inputs from the user (interactive mode)
       M12SQ = SA*CA*(MHH**2 - MHL**2)
       M22SQ = SA**2*MHL**2 + CA**2*MHH**2
       LAMBDA1 = M11SQ/8.D0/VPHI**2
-      LAMBDA2 = (2.D0/DSQRT(3.D0)/VPHI*M12SQ + M1 
+      LAMBDA2 = (2.D0/DSQRT(3.D0)/VPHI*M12SQ + M1
      .     + 4.D0*LAMBDA5*VCHI) / 8.D0 / VCHI
       LAMBDA4 = (M22SQ - M1*VPHI**2/4.D0/VCHI + 6.D0*M2*VCHI
      .     - 8.D0*LAMBDA3*VCHI**2) / 24.D0 / VCHI**2
-      MU3SQ = M1*VPHI**2/4.D0/VCHI + 6.D0*M2*VCHI 
-     .     - (2.D0*LAMBDA2 - LAMBDA5)*VPHI**2 
+      MU3SQ = M1*VPHI**2/4.D0/VCHI + 6.D0*M2*VCHI
+     .     - (2.D0*LAMBDA2 - LAMBDA5)*VPHI**2
      .     - 4.D0*(LAMBDA3 + 3.D0*LAMBDA4)*VCHI**2
-      MU2SQ = -4.D0*LAMBDA1*VPHI**2 
+      MU2SQ = -4.D0*LAMBDA1*VPHI**2
      .     - 3.D0*(2.D0*LAMBDA2 - LAMBDA5)*VCHI**2
      .     + 3.D0/2.D0*M1*VCHI
 
@@ -621,7 +621,7 @@ C Identify the deepest of the good extrema:
 
 C==================================================================
       SUBROUTINE READIN4
-C Reads in the inputs for Input Set 4 and computes the rest of the 
+C Reads in the inputs for Input Set 4 and computes the rest of the
 C Lagrangian parameters in LPARAMS.
 C Input Set 4 is:
 C     mh, m5, sin(thetaH), lambda2, lambda3, lambda4, M1, M2
@@ -663,7 +663,7 @@ C Read inputs from the user (interactive mode)
          PRINT *, " Enter the 8 parameters on one line (no commas): "
          READ *, IMHL, IMH5, ISH, LAMBDA2, LAMBDA3, LAMBDA4, M1, M2
       ENDIF
-      
+
       MHL = IMHL
       MH5 = IMH5
       SH = ISH
@@ -678,17 +678,17 @@ C Read inputs from the user (interactive mode)
       VPHI = DSQRT(V**2 - 8.D0*VCHI**2)
       LAMBDA5 = 2.D0/3.D0/VPHI**2 * (MH5**2 - M1*VPHI**2/4.D0/VCHI
      .     - 12.D0*M2*VCHI - 8.D0*LAMBDA3*VCHI**2)
-      MU3SQ = 2.D0/3.D0*MH5**2 + M1*VPHI**2/12.D0/VCHI 
+      MU3SQ = 2.D0/3.D0*MH5**2 + M1*VPHI**2/12.D0/VCHI
      .     - 2.D0*LAMBDA2*VPHI**2 - 28.D0/3.D0*LAMBDA3*VCHI**2
      .     - 12.D0*LAMBDA4*VCHI**2 - 2.D0*M2*VCHI
 
-      M12SQ = DSQRT(3.D0)/2.D0*VPHI 
+      M12SQ = DSQRT(3.D0)/2.D0*VPHI
      .     * (-M1 + 4.D0*(2.D0*LAMBDA2 - LAMBDA5)*VCHI)
       M22SQ = M1*VPHI**2/4.D0/VCHI - 6.D0*M2*VCHI
      .     + 8.D0*(LAMBDA3 + 3.D0*LAMBDA4)*VCHI**2
-      LAMBDA1 = 1.D0/8.D0/VPHI**2 * (MHL**2 
+      LAMBDA1 = 1.D0/8.D0/VPHI**2 * (MHL**2
      .     + M12SQ**2/(M22SQ - MHL**2))
-      MU2SQ = -4.D0*LAMBDA1*VPHI**2 
+      MU2SQ = -4.D0*LAMBDA1*VPHI**2
      .     - 3.D0*(2.D0*LAMBDA2 - LAMBDA5)*VCHI**2
      .     + 3.D0/2.D0*M1*VCHI
 
@@ -721,7 +721,7 @@ C Identify the deepest of the good extrema:
 
 C==================================================================
       SUBROUTINE READIN5
-C Reads in the inputs for Input Set 5 and computes the rest of the 
+C Reads in the inputs for Input Set 5 and computes the rest of the
 C Lagrangian parameters in LPARAMS.
 C Input Set 5 is:
 C     mh, mH, sin(thetaH), sin(alpha), lambda2, lambda3, lambda4, lambda5
@@ -761,10 +761,10 @@ C Local variables:
 C Read inputs from the user (interactive mode)
          PRINT *
          PRINT *, " Enter the 8 parameters on one line (no commas): "
-         READ *, IMHL, IMHH, ISH, ISA, LAMBDA2, LAMBDA3, 
+         READ *, IMHL, IMHH, ISH, ISA, LAMBDA2, LAMBDA3,
      .        LAMBDA4, LAMBDA5
       ENDIF
-      
+
       MHL = IMHL
       MHH = IMHH
       SH = ISH
@@ -788,14 +788,14 @@ C Read inputs from the user (interactive mode)
       M12SQ = CA*SA*(MHH**2 - MHL**2)
 
       LAMBDA1 = M11SQ/8.D0/VPHI**2
-      M1 = -2.D0*M12SQ/DSQRT(3.D0)/VPHI 
+      M1 = -2.D0*M12SQ/DSQRT(3.D0)/VPHI
      .     + 4.D0*(2.D0*LAMBDA2-LAMBDA5)*VCHI
       M2 = (-M22SQ*VCHI + 8.D0*(LAMBDA3+3.D0*LAMBDA4)*VCHI**3
      .     + M1*VPHI**2/4.D0)/6.D0/VCHI**2
-      MU3SQ = M22SQ - (2.D0*LAMBDA2-LAMBDA5)*VPHI**2 
+      MU3SQ = M22SQ - (2.D0*LAMBDA2-LAMBDA5)*VPHI**2
      .     - 12.D0*(LAMBDA3+3.D0*LAMBDA4)*VCHI**2 + 12.D0*M2*VCHI
-      MU2SQ = -4.D0*LAMBDA1*VPHI**2 
-     .     - 3.D0*(2.D0*LAMBDA2-LAMBDA5)*VCHI**2 
+      MU2SQ = -4.D0*LAMBDA1*VPHI**2
+     .     - 3.D0*(2.D0*LAMBDA2-LAMBDA5)*VCHI**2
      .     + 3.D0/2.D0*M1*VCHI
 
 C Check that the inputted parameters give the right minimum
@@ -827,7 +827,7 @@ C Identify the deepest of the good extrema:
 
 C==================================================================
       SUBROUTINE READIN6
-C Reads in the inputs for Input Set 6 and computes the rest of the 
+C Reads in the inputs for Input Set 6 and computes the rest of the
 C Lagrangian parameters in LPARAMS.
 C Input Set 6 is:
 C     mh, m5, sin(thetaH), lambda2, lambda3, lambda4, lambda5, M2
@@ -867,10 +867,10 @@ C Local variables:
 C Read inputs from the user (interactive mode)
          PRINT *
          PRINT *, " Enter the 8 parameters on one line (no commas): "
-         READ *, IMHL, IMH5, ISH, LAMBDA2, LAMBDA3, LAMBDA4, 
+         READ *, IMHL, IMH5, ISH, LAMBDA2, LAMBDA3, LAMBDA4,
      .        LAMBDA5, M2
       ENDIF
-      
+
       MHL = IMHL
       MH5 = IMH5
       SH = ISH
@@ -888,17 +888,17 @@ C Read inputs from the user (interactive mode)
      .     - 3.D0/DSQRT(2.D0)*LAMBDA5*V*SH
      .     - DSQRT(2.D0)*LAMBDA3*V*SH**3/CH**2
      .     - 6.D0*M2*SH**2/CH**2
-      MU3SQ = 2.D0/3.D0*MH5**2 + M1*VPHI**2/12.D0/VCHI 
+      MU3SQ = 2.D0/3.D0*MH5**2 + M1*VPHI**2/12.D0/VCHI
      .     - 2.D0*LAMBDA2*VPHI**2 - 28.D0/3.D0*LAMBDA3*VCHI**2
      .     - 12.D0*LAMBDA4*VCHI**2 - 2.D0*M2*VCHI
 
-      M12SQ = DSQRT(3.D0)/2.D0*VPHI 
+      M12SQ = DSQRT(3.D0)/2.D0*VPHI
      .     * (-M1 + 4.D0*(2.D0*LAMBDA2 - LAMBDA5)*VCHI)
       M22SQ = M1*VPHI**2/4.D0/VCHI - 6.D0*M2*VCHI
      .     + 8.D0*(LAMBDA3 + 3.D0*LAMBDA4)*VCHI**2
-      LAMBDA1 = 1.D0/8.D0/VPHI**2 * (MHL**2 
+      LAMBDA1 = 1.D0/8.D0/VPHI**2 * (MHL**2
      .     + M12SQ**2/(M22SQ - MHL**2))
-      MU2SQ = -4.D0*LAMBDA1*VPHI**2 
+      MU2SQ = -4.D0*LAMBDA1*VPHI**2
      .     - 3.D0*(2.D0*LAMBDA2 - LAMBDA5)*VCHI**2
      .     + 3.D0/2.D0*M1*VCHI
 
